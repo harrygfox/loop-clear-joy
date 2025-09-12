a42bc3ace9a37d7d3d34807476e544074f12c671
Harry Fox <harryfox@Harrys-MacBook-Pro.local>
2025-09-12 19:29:06 +0100

    number literal on icon files not strings

--- src/components/icons/Exclude.tsx
+++ src/components/icons/Exclude.tsx
@@ -8,7 +8,7 @@ const Exclude: React.FC<Props> = ({ className }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth={2}
     xmlns="http://www.w3.org/2000/svg"
   >
     <path d="M9 3H15L21 9V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V4C3 3.44772 3.44772 3 4 3H9Z"/>
--- src/components/icons/InvoiceReceived.tsx
+++ src/components/icons/InvoiceReceived.tsx
@@ -8,7 +8,7 @@ const InvoiceReceived: React.FC<Props> = ({ className }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth={2}
     xmlns="http://www.w3.org/2000/svg"
   >
     <circle cx="12" cy="12" r="10"/>
